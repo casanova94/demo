5f504f51a18f8e4f625ac1595a912e5badbd7734
Luis Casanova <luis.casanova.uady@live.com>
2017-03-06 12:37:54 -0600

    Se sube el proyecto

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+$destino = "luis.casanova.uady@live.com";
+$nombre = $_POST["Nombre"];
+$correo = $_POST["Correo"];
+$telefono = $_POST["Telefono"];
+$mensaje = $_POST["Mensaje"];
+$contenido = "Nombre: ". $nombre."\nCorreo: ". $correo."\nTeléfono: ".$telefono."\nMensaje: ".$mensaje;
+$state = mail($destino, "Contacto", $contenido);
+header("Location:index.html?sent=1");
+?>
